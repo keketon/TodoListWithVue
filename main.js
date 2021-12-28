608cebbabaf3c0a98a2801260685a561b18fcaa6
keketon <mas08rin10yabko@gmail.com>
2021-12-28 23:02:09 +0900

    doAdd

--- main.js
+++ main.js
@@ -5,15 +5,15 @@ var todoStorage = {
   fetch: function () {
     var todos = JSON.parse(
       localStorage.getItem(STORAGE_KEY) || '[]'
-    )
+    );
     todos.forEach(function (todo, index) {
       todo.id = index
-    })
-    todoStorage.uid = todos.length
-    return todos
+    });
+    todoStorage.uid = todos.length;
+    return todos;
   },
   save: function (todos) {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
   },
 };
 // ストレージAPI
@@ -24,6 +24,18 @@ const app = new Vue({
     todos: []
   },
   methods: {
-    // 使用するメソッド
+    doAdd: function(event, value) {
+      const comment = this.$refs.comment;
+      if (!comment.value.length) {
+        return;
+      }
+
+      this.todos.push({
+        id: todoStorage.uid++,
+        comment: comment.value,
+        state: 0,
+      });
+      comment.value = '';
+    }
   }
 });
