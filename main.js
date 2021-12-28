a234942fa68abc3852b729b879234523853b197f
keketon <mas08rin10yabko@gmail.com>
2021-12-28 22:33:49 +0900

    main.js

---
+++ main.js
@@ -0,0 +1,29 @@
+// https://jp.vuejs.org/v2/examples/todomvc.html
+// ストレージAPI
+var STORAGE_KEY = 'todos-vuejs-demo';
+var todoStorage = {
+  fetch: function() {
+    var todos = JSON.parse(
+      localStorage.getItem(STORAGE_KEY) || '[]'
+    )
+    todos.forEach(function(todo, index) {
+      todo.id = index
+    })
+    todoStorage.uid = todos.length
+    return todos
+  },
+  save: function(todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  },
+};
+// ストレージAPI
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    // 使用するデータ
+  },
+  methods: {
+    // 使用するメソッド
+  }
+});
