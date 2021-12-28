c2f1754a9c735a4830068203ebad0665dd99eac1
keketon <mas08rin10yabko@gmail.com>
2021-12-29 00:03:07 +0900

    display label

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ const app = new Vue({
     current: -1,
   },
   methods: {
-    doAdd: function(event, value) {
+    doAdd: function (event, value) {
       const comment = this.$refs.comment;
       if (!comment.value.length) {
         return;
@@ -43,27 +43,32 @@ const app = new Vue({
       });
       comment.value = '';
     },
-    doChangeState: function(item) {
+    doChangeState: function (item) {
       item.state = item.state ? 0 : 1;
     },
-    doRemove: function(item) {
+    doRemove: function (item) {
       const index = this.todos.indexOf(item);
       this.todos.splice(index, 1);
     }
   },
   watch: {
     todos: {
-      handler: function(todos) {
+      handler: function (todos) {
         todoStorage.save(todos);
       },
       deep: true,
     },
   },
   computed: {
-    computedTodos: function() {
-      return this.todos.filter(function(el) {
+    computedTodos: function () {
+      return this.todos.filter(function (el) {
         return this.current < 0 ? true : this.current === el.state;
       }, this);
+    },
+    labels() {
+      return this.options.reduce(function (prv, cur) {
+        return Object.assign(prv, { [cur.value]: cur.label });
+      }, {});
     },
   },
   created() {
